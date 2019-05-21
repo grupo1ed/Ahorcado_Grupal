e1c2cc42c12bf085b261e049114cbd2d9c67c7da
martolomiu <martolomiu15@gmail.com>
2019-05-21 11:12:04 +0200

    palabraAleatoria estoy intentando sacar una palabra aleatoria devuelta en un metodo.

--- Ahorcado/palabras_ahorcado.c
+++ Ahorcado/palabras_ahorcado.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <math.h>
 
 char * Palabra();
 
@@ -10,8 +14,10 @@ void main(){
 }
 
 char * Palabra(){
- static char palabra[4] = "oso";
-// return palabra[1];
-return palabra;
+  char *lista[10] = {"oso", "amigo", "estornudo", "microondas", "continuacion", "aminoacido", "algoritmo", "inteligencia", "mentecato", "procesos"};
+  srand(time(NULL));
+  float numero = rand() % (9 - 0) + 0;
+  int redondeo = roundf(numero);
+  char palabra[50]; strcpy(palabra,lista[redondeo]);
+  return palabra;
 }
-
