30a85c9b31c8c57b101a227ee505727dda03eb0a
martolomiu <martolomiu15@gmail.com>
2019-05-21 10:54:36 +0200

    implementarRandom como un programa modular que devuelva un string (entre todos).

--- Ahorcado/palabras_ahorcado.c
+++ Ahorcado/palabras_ahorcado.c
@@ -7,11 +7,26 @@ void main(){
 
  char *palabra=Palabra();
  printf("\n%s\n", palabra);
+ /*
+
+  char* letra = generaPalabra(lista);
+  srand(time(NULL));
+  float numero = rand() % (9 - 0) + 0;
+  int redondeo = roundf(numero);
+  char palabra[50]; strcpy(palabra,lista[redondeo]);
+
+  char tecla;
+  int cambios = 0;
+  static char comprobador [100];
+  static int intentos = 4;
+  int longitud = strlen(palabra);
+  system("cls");
+  */
 }
 
 char * Palabra(){
- static char palabra[4] = "oso";
+  char *lista[10] = {"oso", "amigo", "estornudo", "microondas", "continuacion", "aminoacido", "algoritmo", "inteligencia", "mentecato", "procesos"};
+  static char palabra[4] = "oso";
 // return palabra[1];
 return palabra;
 }
-
